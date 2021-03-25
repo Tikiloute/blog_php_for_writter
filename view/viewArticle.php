fab9a0c8a53388b5ca2dac80bc61a97fb7b07523
Bruno Etcheverry <bruno.etcheverry@hotmail.fr>
2021-03-25 12:29:38 +0100

    ajout du lien, pas encore d'ajout des textes area qui permettront d'update les données des articles une fois modifiés

--- view/viewArticle.php
+++ view/viewArticle.php
@@ -7,6 +7,7 @@ $aimArticle = $_GET['read'] - 1;
         <div class="card-body">
             <h5 class="card-title"><?php echo $articles[$aimArticle]['titre'] ?></h5>
             <p class="card-text"><?php echo $articles[$aimArticle]['contenu'] ?></p>
+            <a href="index.php?modify=<?php echo $articles[$aimArticle]['id']?>" class="btn btn-primary d-grid col-3 mx-auto">Modifier l'article</a>
         </div>
     </div>
 <?php 
